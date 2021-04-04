b8e2fef922281b6983c089f41feaa63bcaac7e56
Ferran00 <ferran.iglesias.barenys@estudiantat.upc.edu>
2021-04-04 17:47:06 +0000

    arreglar un if de submit_controller, i afegir el if de si té url AND text

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -9,13 +9,21 @@ class SubmitController < ApplicationController
     if !params[:title].blank? && (!params[:url].blank? || !params[:text].blank?)
       
       @existingNewsWithSameURL = New.where(url: params[:url]).first
-      if @existingNewsWithSameURL.blank?  #si no hi ha cap altre "new" amb aquest URL
+      if params[:url].blank? || @existingNewsWithSameURL.blank?  #si no hi ha cap altre "new" amb aquest URL ó no s'ha introduit url
       
         if (!params[:url].blank?) then isurl=1;
         else isurl=0
         end
+        
+        if !params[:url].blank? && !params[:text].blank? #si té url AND text
+          @new = New.new(title: params[:title], url: params[:url], text: "", isurl: isurl, points: params[:points])
+          #i ara falta posar el text com a primer comentari.
+        else
           @new = New.new(title: params[:title], url: params[:url], text: params[:text], isurl: isurl, points: params[:points])
-          @new.save
+        end
+        
+        @new.save
+        
         redirect_to newest_path
         
       else  #si ja existeix una publicacio amb aquest url
