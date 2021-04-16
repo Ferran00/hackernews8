d847fca964030030c7485dfd1680663088c07375
Ubuntu <ubuntu@ip-172-31-41-239.ec2.internal>
2021-04-16 16:22:59 +0000

    Gemfile Updated

--- app/controllers/new_controller.rb
+++ app/controllers/new_controller.rb
@@ -1,6 +1,24 @@
 class NewController < ApplicationController
   def index
     @new = New.order('created_at DESC').all
+    if !current_user.nil?
+      @userliked = Likenew.where(user_id: session.user_id).all
+    end
     @paginanewest = true
+  end
+  
+  def vote
+    #comprobar sesion
+    if !current_user.nil?
+      Likenew.new(user_id: params[:userid], new_id: params[:newid])
+      redirect_to :new
+    end
+  end    
+  
+  def unvote
+    if !current_user.nil?
+      Likenew.where(:user_id => params[:userid]).where(:new_id => params[:newid]).delete
+      redirect_to :new
+    end
   end
 end
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -8,4 +8,34 @@ class NewsController < ApplicationController
     @ask = New.find(params[:id])
     @comments = Comment.where("new_id == @ask.id").all#.count
   end
+  
+  def vote
+    Likenew.new(user_id: params[:userid], new_id: params[:newid])
+    redirect_to :news
+  end    
+  
+  def unvote
+    Likenew.where(:user_id => params[:userid]).where(:new_id => params[:newid]).delete
+    redirect_to :news
+  end
+  
+  def vote_item
+    Likenew.new(user_id: params[:userid], new_id: params[:newid])
+    redirect_to :item
+  end    
+  
+  def unvote_item
+    Likenew.where(:user_id => params[:userid]).where(:new_id => params[:newid]).delete
+    redirect_to :item
+  end
+  
+  def vote_comment
+    Likecomment.new(user_id: params[:userid], comment_id: params[:commentid])
+    redirect_to :item
+  end    
+  
+  def unvote_comment
+    Likecomment.where(:user_id => params[:userid]).where(:comment_id => params[:commentid]).delete
+    redirect_to :item
+  end
 end
