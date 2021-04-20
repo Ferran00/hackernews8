509f41e3ea8ec3afe3208dde1b80ffce938ca6b4
Ubuntu <ubuntu@ip-172-31-41-239.ec2.internal>
2021-04-20 12:29:36 +0000

    mas pruebas login4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
-def googleAuth
-    if @current_user.nil?
-        # Get access tokens from the google server
-        access_token = request.env["omniauth.auth"]
-        user = User.from_omniauth(access_token)
-        
-        session[:user_id] = access_token["uid"]
-        @current_user = @user_id
-        
-        # log_in(user)
-        # Access_token is used to authenticate request made from the rails application to the google server
-        user.google_token = access_token.credentials.token
-        # Refresh_token to request new access_token
-        # Note: Refresh_token is only sent once during the first request
-        refresh_token = access_token.credentials.refresh_token
-        user.google_refresh_token = refresh_token if refresh_token.present?
-        user.save
-    end
-    redirect_to root_path
+    def googleAuth
+        # if @current_user.nil?
+            # Get access tokens from the google server
+            access_token = request.env["omniauth.auth"]
+            user = User.from_omniauth(access_token)
+            
+            session[:user_id] = access_token["uid"]
+            @current_user = @user_id
+            
+            # log_in(user)
+            # Access_token is used to authenticate request made from the rails application to the google server
+            user.google_token = access_token.credentials.token
+            # Refresh_token to request new access_token
+            # Note: Refresh_token is only sent once during the first request
+            refresh_token = access_token.credentials.refresh_token
+            user.google_refresh_token = refresh_token if refresh_token.present?
+            user.save
+        # end
+        redirect_to '/'
 end
