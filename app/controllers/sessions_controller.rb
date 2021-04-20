5d3e009c5f4a53d17f26b3dba7343bcda148cf00
Ubuntu <ubuntu@ip-172-31-41-239.ec2.internal>
2021-04-20 11:43:47 +0000

    mas pruebas login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,21 @@
 def googleAuth
-    # Get access tokens from the google server
-    access_token = request.env["omniauth.auth"]
-    user = User.from_omniauth(access_token)
-    
-    session[:user_id] = access_token["uid"]
-    @current_user = @user_id
-    
-    # log_in(user)
-    # Access_token is used to authenticate request made from the rails application to the google server
-    # user.google_token = access_token.credentials.token
-    # Refresh_token to request new access_token
-    # Note: Refresh_token is only sent once during the first request
-    # refresh_token = access_token.credentials.refresh_token
-    # user.google_refresh_token = refresh_token if refresh_token.present?
-    user.save
+    if !@current_user.nil?
+        # Get access tokens from the google server
+        access_token = request.env["omniauth.auth"]
+        user = User.from_omniauth(access_token)
+        
+        session[:user_id] = access_token["uid"]
+        @current_user = @user_id
+        
+        # log_in(user)
+        # Access_token is used to authenticate request made from the rails application to the google server
+        user.google_token = access_token.credentials.token
+        # Refresh_token to request new access_token
+        # Note: Refresh_token is only sent once during the first request
+        refresh_token = access_token.credentials.refresh_token
+        user.google_refresh_token = refresh_token if refresh_token.present?
+        user.
+    end
     redirect_to root_path
 end
-
+end
