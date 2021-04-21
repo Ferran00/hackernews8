2859899e47d44ab2d358740444d7a1378cb09740
Ubuntu <ubuntu@ip-172-31-41-239.ec2.internal>
2021-04-21 11:40:22 +0000

    Login Funcional - Output tests eliminados

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,33 +1,23 @@
 class SessionsController < ApplicationController
     def googleAuth
-        # if @current_user.nil?
-            # Get access tokens from the google server
-            user_info = request.env["omniauth.auth"]
-            temp_id = (user_info.uid.to_i % 1048576)
-            user_info.uid = temp_id
-            user = User.from_omniauth(user_info)
+        # Get access tokens from the google server
+        user_info = request.env["omniauth.auth"]
+        temp_id = (user_info.uid.to_i % 1048576)
+        user_info.uid = temp_id
+        user = User.from_omniauth(user_info)
 
-            # @current_user = session[:user_id]
-            
-            # log_in(user)
-            # Access_token is used to authenticate request made from the rails application to the google server
-            user.google_token = user_info.credentials.token
-            # Refresh_token to request new user_info
-            # Note: Refresh_token is only sent once during the first request
-            refresh_token = user_info.credentials.refresh_token
-            user.google_refresh_token = refresh_token if refresh_token.present?
-            puts user_info.uid
-            user.id = temp_id
-            puts temp_id
-            user.save
+        # Access_token is used to authenticate request made from the rails application to the google server
+        user.google_token = user_info.credentials.token
+        # Refresh_token to request new user_info
+        # Note: Refresh_token is only sent once during the first request
+        refresh_token = user_info.credentials.refresh_token
+        user.google_refresh_token = refresh_token if refresh_token.present?
+        user.id = temp_id
+        user.save
 
-            session[:user_id] = temp_id
+        session[:user_id] = temp_id
+        @current_user = session[:user_id]
 
-            @current_user = session[:user_id]
-
-
-            
-        # end
         redirect_to root_path
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,9 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     # Creates a new user only if it doesn't exist
     where(email: auth.info.email).first_or_initialize do |user|
-                  puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
       user.id = auth.uid
-      puts auth.uid
-      puts user.id
       user.username = auth.info.name
       user.email = auth.info.email
-      puts user.username
-      puts user.email
     end
   end
 end
