d7148c97d73907c9738982f7bd989c741e59d208
Ubuntu <ubuntu@ip-172-31-14-177.ec2.internal>
2021-04-22 18:30:12 +0000

    comments functional

--- app/controllers/otherprofile_controller.rb
+++ app/controllers/otherprofile_controller.rb
@@ -5,6 +5,7 @@ class OtherprofileController < ApplicationController
     @created = @actual_user.created_at.strftime("%B %d, %Y")
     @karma = @actual_user.karma
     @about = @actual_user.about
+    @id = params[:userid]
   end
   
   render "otherprofile/index"
