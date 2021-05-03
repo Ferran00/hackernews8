fadc109711a06068ae5b5cde401c5be7358224fd
Ferran00 <ferran.iglesias.barenys@estudiantat.upc.edu>
2021-05-03 09:31:19 +0000

    +++ arribo a

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -1,8 +1,13 @@
 class Api::NewsController < ApplicationController
   
+    puts "*******************************Arribo a la classe NewsController"
+  
   def getInfoNew
     
+    puts "*******************************Arribo a NewsController#getInfoNew"
+    
     respond_to do |format|
+    puts "*******************************Arribo al respond"
       if New.exists?(params[:id])
         @new = New.find(params[:id])
         format.json { render json: @new, status: :ok}
