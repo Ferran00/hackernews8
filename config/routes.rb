f22455463344ea300f356d4ddd539802171808c7
Ubuntu <ubuntu@ip-172-31-14-177.ec2.internal>
2021-05-05 18:59:07 +0000

    other profile 2

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ Rails.application.routes.draw do
     #users
     get "/users/profile", to: "api/users#getProfile"
     put "/users/profile", to: "api/users#updateProfile"
-    get "/users/otherprofile/:email", to: "api/users#getOtherProfile"
+    get "/users/otherprofile", to: "api/users#getOtherProfile"
     
     #comments
     post "/reply/:comment_id", to: "api/replies#createReply"
