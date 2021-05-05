b5c7d8798692aaf7bf05e64dc5850b8b49e7b8b1
Ubuntu <ubuntu@ip-172-31-14-177.ec2.internal>
2021-05-05 08:46:34 +0000

    users controller: getprofile and updateprofile

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,37 @@
+class Api::UsersController < ApplicationController
+  
+  def getProfile
+    respond_to do |format|  
+      if request.headers['token'].present?
+        @key = request.headers['token'].to_s
+        if User.exists?(api_key: @key)
+          @user  = User.find(api_key: @key)
+          format.json { render json: { id: @user.id, username: @user.email, karma: @user.karma, about: @user.about, created_at: @user.created_at, api_key: @user.api_key}}
+        else
+          format.json { render json: {error: "error", code: 404, message: "The user with token: " + @key + " doesn't exist"}, status: :not_found}
+        end
+      else
+        format.json { render json:{status:"error", code:403, message: "The autentication token is not provided"}, status: :forbidden}
+      end
+    end
+  end
+  
+  def updateProfile
+    respond_to do |format|
+      if request.headers['token'].present?
+        @key = request.headers['token'].to_s
+        if User.exists?(api_key: @key)
+          @user  = User.find(api_key: @key)
+          @user.about = params[:about]
+          @user.email = params[:email]
+          @user.save
+          format.json { render json: {id: @user.id, username: @user.email, karma: @user.karma, about: @user.about, created_at: @user.created_at, api_key: @user.api_key}, status: :ok}
+        else
+          format.json { render json: {error: "error", code: 404, message: "The user with token: " + @key + " doesn't exist"}, status: :not_found}
+        end
+      else
+        format.json { render json:{status:"error", code:403, message: "The autentication token is not provided"}, status: :forbidden}
+      end
+    end
+  end
+end
