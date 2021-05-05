2b2f1e86f4d836f1e9ef0d0e3519fe2dee5385c1
Ubuntu <ubuntu@ip-172-31-14-177.ec2.internal>
2021-05-05 09:15:46 +0000

    pruebas profile

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,16 +1,20 @@
 class Api::UsersController < ApplicationController
-  
+  puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA 1"
   def getProfile
     respond_to do |format|  
       if request.headers['token'].present?
+          puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA 2"
         @key = request.headers['token'].to_s
         if User.exists?(api_key: @key)
+            puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA 3"
           @user  = User.find(api_key: @key)
           format.json { render json: { id: @user.id, username: @user.email, karma: @user.karma, about: @user.about, created_at: @user.created_at, api_key: @user.api_key}}
         else
+          puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA 4"
           format.json { render json: {error: "error", code: 404, message: "The user with token: " + @key + " doesn't exist"}, status: :not_found}
         end
       else
+        puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA 5"
         format.json { render json:{status:"error", code:403, message: "The autentication token is not provided"}, status: :forbidden}
       end
     end
