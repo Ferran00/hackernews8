f33c2cdda2608061a3e1eb71b145e0bab2f15c1d
Ubuntu <ubuntu@ip-172-31-33-164.ec2.internal>
2021-05-12 15:08:16 +0000

    Standard API correction try2

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -17,21 +17,25 @@ class Api::UsersController < ApplicationController
   
   def getOtherProfile
     respond_to do |format|  
-      if !params[:email].nil?
-        if request.headers['token'].present?
-          @key = request.headers['token'].to_s
-          if User.exists?(api_key: @key)
+      # if !params[:email].nil?
+      if request.headers['token'].present?
+        @key = request.headers['token'].to_s
+        if User.exists?(api_key: @key)
+          if User.exists?(email: params[:email])
             @user  = User.find_by(email: params[:email])
             format.json { render json: {username: @user.username, created_at: @user.created_at, karma: @user.karma, about: @user.about, email: @user.email}}
           else
-            format.json { render json:{status:"error", code:401, message: "Invalid API key"}, status: :unauthorized}
-          end
+            format.json { render json:{status:"error", code:404, message: "User with email '" + params[:email] + "' not found"}, status: :not_found}
+          end  
         else
-          format.json { render json:{status:"error", code:401, message: "The authentication token is not provided"}, status: :unauthorized}
+          format.json { render json:{status:"error", code:401, message: "Invalid API key"}, status: :unauthorized}
         end
-      else 
-        format.json { render json:{status:"error", code:400, message: "No email specified (query)"}, status: :bad_request}
+      else
+        format.json { render json:{status:"error", code:401, message: "The authentication token is not provided"}, status: :unauthorized}
       end
+      # else 
+        # format.json { render json:{status:"error", code:400, message: "No email specified (query)"}, status: :bad_request}
+      # end
     end
   end
   
