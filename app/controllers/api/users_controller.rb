6c89fe3fa884353e4ce54ede3595f193768be709
Ubuntu <ubuntu@ip-172-31-14-177.ec2.internal>
2021-05-12 09:21:37 +0000

    get submissions fix 3

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -62,9 +62,8 @@ class Api::UsersController < ApplicationController
       if request.headers['token'].present?
         @key = request.headers['token'].to_s
         if User.exists?(api_key: @key)
-          @email  = params[:email]
-          @user = User.find_by(email: @email)
-          @otherUserNews = New.where(user_id: @user.id).order('points DESC').all
+          @user = User.find_by(:email => params[:email])
+          @otherUserNews = New.where(:user_id => @user.id).order('points DESC').all
           format.json { render json: @otherUserNews, status: :ok}
         else
           format.json { render json: {error: "error", code: 404, message: "The user with token: " + @key + " doesn't exist"}, status: :not_found}
